bf2c7e1eb15104f330d06349c15c0886881183b6
valentina-RGB <valensc2545@gmail.com>
2024-09-25 16:50:18 -0500

    IA_LISTA

--- BACKEND/src/controllers/IA.js
+++ BACKEND/src/controllers/IA.js
@@ -8,10 +8,30 @@ const axios = require('axios')
   
 const chatWithGPT = async (req = request, res= response)=> {
 
-let prompt = "puedes darme una definicion de que es un json"
+let {stratum, members, account,cubic_meters,L_shower,reasoning, L_faucet, L_wash, washingMachine,carWaterUsage,beefWaterUsage,milkWaterUsage,sodaWaterUsage,costPerCubicMeter,waterUsagePerRationingDay} = req.body
+let prompt = `Me gustaria reducir mi consumo de agua teniendo en cuenta los siguientes puntos 
+1. Vivo en bogotá y soy estracto ${stratum}, 
+2. Somos ${members} en mi nucleo familiar,
+3. Tengo ${reasoning}días de razonamiento de agua por lo que no tengo agua todo el mes,
+4. Utilizo ${cubic_meters}metros cubicos por mes según mi factura,
+5. El valor de mi cuenta este mes fue ${account},
+6. Gasto un total de ${L_shower} litros por minuto de ducha,
+7. Gasto un total de ${L_faucet} litros por minuto del grifo,
+8. Gasto un total de ${L_wash} litros por lavado de platos,
+9. Gasto un total de ${washingMachine} litros por lavado de ropa,
+10. Gasto un total de ${carWaterUsage} por lavado del coche,
+11. Gasto un total de ${beefWaterUsage} por kilogramo de res,
+12. Gasto un total de ${milkWaterUsage} litros por litro de leche,
+13. Gasto un total de ${sodaWaterUsage} por litro de refresco,
+14. El costos por metro cubico de agua en mi hogar es de ${costPerCubicMeter},
+15. Mi consumo promedio durante los días de racionamiento: ${waterUsagePerRationingDay.toFixed(2)} litros por día
+
+En los tips que me vayas a dar por favor incluye cuanto puede reducir mi consumo en litros y en pesos colombianos `
 
 
-  axios.post(`${process.env.CHATGPT}/${prompt}`)
+let encodedPrompt = encodeURIComponent(prompt);
+
+  axios.post(`${process.env.CHATGPT}/${encodedPrompt}`)
   .then(response => {
     console.log('Respuesta del servidor:', response.data);
   })
